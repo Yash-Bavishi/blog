72f2d41798575a396e1893b1810659b5378ee71f
Yash Bavishi <bavishiys63@gmail.com>
2022-12-02 20:08:04 +0530

    UI complete

--- front-end/src/components/Blog.jsx
+++ front-end/src/components/Blog.jsx
@@ -13,21 +13,40 @@ function Blog(props) {
         </div>
       </div>
         */
+
+  /*
+  
+      <div >
+        <Link to="/blogs" className="test" state={{ from: props }}> <h1>Link</h1> </Link>
+        <h1>{props.blog.title}</h1>
+        <div className="card">
+          <h3>{props.blog.author}</h3>
+          <div>
+            <p>{props.blog.description}</p>
+          </div>
+          <p>{props.blog.url}</p>
+        </div>
+      </div >
+  */
+
   console.log(props.blog)
 
 
   return (
-    <div >
-      <Link to="/blogs" className="test" state={{ from: props }}> Link to page </Link>
-      <h1>{props.blog.title}</h1>
-      <div className="card">
-        <h3>{props.blog.author}</h3>
-        <div>
-          <p>{props.blog.description}</p>
+
+    <Link to="/blogs" className="test" state={{ from: props }}>
+      <div className="blog_container">
+        <div className="content-pad">
+          <h1 className="b1">{props.blog.title}</h1>
+          <hr />
+          <ul>
+            <li>
+              <p className="blogger">{props.blog.description}</p>
+            </li>
+          </ul>
         </div>
-        <p>{props.blog.url}</p>
       </div>
-    </div >
+    </Link>
   )
 }
 
